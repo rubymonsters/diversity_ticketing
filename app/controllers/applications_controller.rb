d4fe8234457626bb912128cbdd389595bd590c7a
Katrin Marschner <katrin.marschner@gmail.com>
2015-08-11 19:31:51 +0200

    finish save new applications

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,18 @@ class ApplicationsController < ApplicationController
 	def new
 		 @event = Event.find(params[:event_id])
 	end
+
+  def create
+    @event = Event.find(params[:event_id])
+
+    @application = Application.new(application_params)
+    @application.event_id = params[:event_id]
+    @application.save
+    redirect_to @event
+  end
+
+  private
+    def application_params
+      params.require(:application).permit(:name, :email, :answer_1, :answer_2, :answer_3)
+    end
 end
