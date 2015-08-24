b9e2d074c46ba6492e65872047fd942f10cc4f37
Katrin Marschner <katrin.marschner@gmail.com>
2015-08-24 21:36:30 +0200

    add organizer email validationn

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,9 @@
 class Event < ActiveRecord::Base
   has_many :applications
-  validates :organizer_name, :organizer_email, :description, :name, :question_1, presence: true
+  
+  validates :organizer_name, :description, :name, :question_1, presence: true
   validates :start_date, date: true
   validates :end_date, date: { after: :start_date }
-  validates :organizer_email, confirmation: true
+  validates :organizer_email, confirmation: true, format: { with: /.+@.+\..+/ }, presence: true
   validates :organizer_email_confirmation, presence: true
 end
