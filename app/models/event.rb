5d78d5d470b1d9556785bc0a2fe8aa59b51b4a32
Katrin Marschner <katrin.marschner@gmail.com>
2015-08-24 20:08:37 +0200

    add event validations, add gem date_validator

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
   has_many :applications
+  validates :organizer_name, :organizer_email, :description, :name, :question_1, presence: true
+  validates :start_date, date: true
+  validates :end_date, date: { after: :start_date }
+  validates :organizer_email, confirmation: true
+  validates :organizer_email_confirmation, presence: true
 end
