54d31c1e6b62247c68d1dd0263c6dded40fef445
Powerglove <lang.joh.jl@gmail.com>
2015-08-31 20:28:00 +0200

    create admin area

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,10 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def admin_index
+    @events = Event.all
+  end
+
   def show
     @event = Event.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :events do 
     resources :applications
   end
+
+  get '/admin', to: 'events#admin_index'
    #The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
