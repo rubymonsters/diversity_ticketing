a70305f3a5d21b9d1e4041a016a10f5afb27a1db
Katrin Marschner <katrin.marschner@gmail.com>
2015-10-06 19:30:04 +0200

    take away questions and answers from views

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   has_many :applications
   
-  validates :organizer_name, :description, :name, :question_1, presence: true
+  validates :organizer_name, :description, :name, presence: true
   validates :start_date, date: true
   validates :end_date, date: { after: :start_date }
   validates :organizer_email, confirmation: true, format: { with: /.+@.+\..+/ }, presence: true
