569bbfc7757914193c934c26aa198b600903f016
sabrina-ulbrich <su@evolos.de>
2015-11-16 22:09:55 +0100

    add Event#open? and email validation tests
    decide to use describe test format
    
    add a test for Event#open? method (happy and sad paths)
    
    start writing test for email validation (happy and sad paths)
    
    add helper methods assert_attribute_valid and assert_attribute_invalid

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  def test_open_returns_true_for_future_end_date
-    event = Event.new(end_date: 1.week.from_now)
-    assert true, event.open?
-  end
-
-  test 'open? returns true when the end date is in the future' do
-    event = Event.new(end_date: 1.week.from_now)
-    assert true, event.open?
-  end
-
-  describe '#open?' do
+  describe "#open?" do
     it 'returns true when the end date is in the future' do
       event = Event.new(end_date: 1.week.from_now)
-      assert true, event.open?
+      assert_equal true, event.open?
     end
 
-    it 'does something else'
+    it 'returns false when the end date is in the past' do
+      event = Event.new(end_date: 1.week.ago)
+      assert_equal false, event.open?
+    end
+  end
+
+  describe "validating organizer_email" do
+    it 'should allow a valid email address' do
+      event = Event.new(organizer_email: "email@website.de")
+      assert_attribute_valid(event, :organizer_email)
+    end
+
+    it 'should not allow an invalid email address' do
+      event = Event.new(organizer_email: "emailwebsite.de")
+      assert_attribute_invalid(event, :organizer_email)
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,8 +8,21 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+module Minitest::Assertions
   def admin_login
     username, password = ENV['DT_USERNAME'], ENV['DT_PASSWORD']
     @request.headers['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(username, password)
+  end
+
+  def assert_attribute_valid(event, attribute)
+    event.valid?
+    assert_equal [], event.errors[attribute]
+  end
+
+  def assert_attribute_invalid(event, attribute)
+    event.valid?
+    refute_equal [], event.errors[attribute]
   end
 end
