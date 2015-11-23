68f9203fe7a4f8a58a4a42ff8df41cd503c9c733
Ferdous <itrytocode@gmail.com>
2015-11-23 21:42:43 +0100

    Test(applications-controller): add new tests

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -1,10 +1,8 @@
 require 'test_helper'
 
 class ApplicationsControllerTest < ActionController::TestCase
-  test 'shows application to admin users' do
-    admin_login
-
-    event = Event.create!(
+  def make_event
+    Event.create!(
       name: 'Event',
       start_date: '2015-12-20',
       end_date: '2015-12-22',
@@ -19,8 +17,10 @@ class ApplicationsControllerTest < ActionController::TestCase
       deadline: '2015-12-01',
       number_of_tickets: 10
     )
+  end
 
-    application = Application.create!(
+  def make_application(event)
+    Application.create!(
       name: 'Joe',
       email: 'joe@test.com',
       email_confirmation: 'joe@test.com',
@@ -29,8 +29,64 @@ class ApplicationsControllerTest < ActionController::TestCase
       answer_2: 'Hi',
       answer_3: 'Hi'
     )
+  end
+
+  # GET show
+  test 'shows application to admin users' do
+    admin_login
+
+    event = make_event
+    application = make_application(event)
 
     get :show, event_id: event.id, id: application.id
+
+    assert_response :success
+  end
+
+  test 'does not show application to non-admin users' do
+    event = make_event
+    application = make_application(event)
+
+    get :show, event_id: event.id, id: application.id
+
+    assert_response 401
+  end
+
+  test 'raise exception if application does not exist' do
+    admin_login
+
+    event = make_event
+
+    assert_raises(ActiveRecord::RecordNotFound) do
+      get :show, event_id: event.id, id: 1
+    end
+  end
+
+  # POST create
+  test 'proper redirection after successful application' do
+      event = make_event
+
+      post :create, event_id: event.id,
+                    application: {
+                      name: 'Joe',
+                      email: 'joe@test.com',
+                      email_confirmation: 'joe@test.com',
+                      event: event,
+                      answer_1: 'Hi',
+                      answer_2: 'Hi',
+                      answer_3: 'Hi'
+                    }
+
+      assert_redirected_to event
+  end
+
+  test 'no redirection after failed validations' do
+    event = make_event
+
+    post :create, event_id: event.id,
+                  application: {
+                    name: 'Joe'
+                  }
 
     assert_response :success
   end
