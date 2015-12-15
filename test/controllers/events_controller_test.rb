f72593e2932b3c6c8a5983fdb83128586fad7cf0
Powerglove <lang.joh.jl@gmail.com>
2015-12-15 17:49:34 +0100

    add test for Event#index: shows only future events

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -11,4 +11,15 @@ class EventsControllerTest < ActionController::TestCase
 
   	assert(css_select("h3").none? { |element| element.text == unapproved_event.name })
   end
+
+  test "index action shows only future events" do
+ 		future_event = make_event(approved: true)
+ 		past_event = make_event(start_date: 1.week.ago, end_date: 1.week.ago, deadline: 2.weeks.ago, approved: true, name: 'Other')
+
+ 		get :index
+
+  	assert_select "h3", {count: 1, text: future_event.name}
+
+  	assert(css_select("h3").none? { |element| element.text == past_event.name })
+ 	end
 end
