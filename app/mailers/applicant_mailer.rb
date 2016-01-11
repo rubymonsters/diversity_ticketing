8f12d3989173d19a44bf8c65d9d3195dfccef366
ariaru <ariaru@hotmail.de>
2016-01-11 21:02:39 +0100

    Add Anika's mail text

--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -2,7 +2,7 @@ class ApplicantMailer < ApplicationMailer
 
 	def application_received(application)
 		@application = application
-    mail(to: @application.email, subject: "Thanks for applying for #{@application.event.name}.")
+    mail(to: @application.email, subject: "Your application for #{@application.event.name}.")
 	end
-	
+
 end
