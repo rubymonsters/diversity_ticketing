446a4f41b9c86f5667538288572c2fd3dabe7a5c
aydamacink <dayanamick@gmail.com>
2016-01-18 21:44:00 +0100

    Revert "Add checkboxes to application"

--- db/migrate/20160118193813_add_checkboxes_to_applicants.rb
+++
@@ -1,6 +0,0 @@
-class AddCheckboxesToApplicants < ActiveRecord::Migration
-  def change
-    add_column :applications, :ticket_needed, :boolean, null: false, default: false
-    add_column :applications, :travel_needed, :boolean, null: false, default: false
-  end
-end
