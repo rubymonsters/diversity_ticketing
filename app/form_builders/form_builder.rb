4ce0ceb8246c0f22970d9b34052b5be2bd649c11
Katrin Marschner <katrin.marschner@gmail.com>
2016-02-22 21:51:54 +0100

    make form builder for file upload with image, use for event

--- app/form_builders/form_builder.rb
+++ app/form_builders/form_builder.rb
@@ -1,13 +1,40 @@
 class FormBuilder < ActionView::Helpers::FormBuilder
-  def form_field(field_type, attribute, name, options = {})
-    css_classes = options.delete(:class) || []
-    css_classes << 'form_field'
-    if @object.class.validators_on(attribute).any? { |v| v.kind == :presence }
-      css_classes << 'required'
-    end
+  include ActionView::Helpers::CaptureHelper
 
-    @template.content_tag(:div, class: css_classes) do
-      label(attribute, name) + send(field_type, attribute, objectify_options(options))
+  def form_field(field_type, attribute, name, options = {})
+    class_option = options.delete(:class)
+    t = @template
+
+    t.content_tag(:div, class: css_classes(class_option, attribute)) do
+      t.capture do
+        t.concat label(attribute, name)
+        t.concat send(field_type, attribute, objectify_options(options))
+      end
     end
+  end
+
+  def image_upload_field(attribute, name, options = {})
+    class_option = options.delete(:class)
+    image_name = options.delete(:image_name)
+    t = @template
+
+    t.content_tag(:div, class: css_classes(class_option, attribute)) do
+      t.capture do
+        t.concat label(attribute, name)
+        t.concat(t.content_tag(:p) {
+          t.image_tag(image_name, class: "event-logo")
+        })
+        t.concat file_field(attribute, objectify_options(options))
+      end
+    end
+  end
+
+  def css_classes(class_option, attribute)
+    classes = class_option || []
+    classes << 'form_field'
+    if @object.class.validators_on(attribute).any? { |v| v.kind == :presence }
+      classes << 'required'
+    end
+    classes
   end
 end
