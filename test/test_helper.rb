7e41939db1aa63d5568cfb8676702e68e29a7377
ariaru <ariaru@hotmail.de>
2016-03-21 21:25:34 +0100

    Fix broken tests from adding clearance gem
    add clearance test helper
    replace obsolete helper methods
    use clearance's paths

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,14 +8,11 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/autorun'
+require 'clearance/test_unit'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  def admin_login
-    session[:authenticated] = true
-  end
 
   def make_event(event_params = {})
     defaults = {
