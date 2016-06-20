3e244ed736020a6f46b35167c472870bd107d1d7
Katrin Marschner <katrin.marschner@gmail.com>
2016-06-20 20:01:37 +0200

    use fake credentials if no ENVs

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,3 +1,9 @@
+if Rails.env.test? || Rails.env.development?
+  ENV['AWS_ACCESS_KEY_ID'] ||= '123456'
+  ENV['AWS_SECRET_ACCESS_KEY'] ||= '123456'
+  ENV['S3_BUCKET'] ||= '1234556'
+end
+
 Aws.config.update({
   region: 'eu-central-1',
   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY']),
