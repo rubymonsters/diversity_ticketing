3e96bb211660146dc657cc07c21b6417c88575dc
Katrin Marschner <katrin.marschner@gmail.com>
2016-07-03 15:03:17 +0200

    show event on the end date

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,7 +9,7 @@ class Event < ActiveRecord::Base
   validates :organizer_email_confirmation, presence: true, on: :create
   validates :website, :code_of_conduct, format: { with: /(http|https):\/\/.+\..+/ }
   validates :number_of_tickets, numericality: { only_integer: true, greater_than_or_equal_to: 1 }, presence: true
-  
+
   def self.approved
     where(approved: true)
   end
@@ -19,7 +19,7 @@ class Event < ActiveRecord::Base
   end
 
   def self.upcoming
-    where('end_date > ?', DateTime.now)
+    where('end_date >= ?', DateTime.now)
   end
 
   def self.past
