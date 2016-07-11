7ac41921693d2d6ea6bf512d41d5ea65663047ac
ariaru <ariaru@hotmail.de>
2016-07-11 19:50:49 +0200

    convert tabs to spaces

--- test/models/application_process_test.rb
+++ test/models/application_process_test.rb
@@ -1,18 +1,17 @@
 require 'test_helper'
 
 class ApplicationProcessTest < ActiveSupport::TestCase
+  describe 'handling application process' do
+    it 'is valid if its value is allowed' do
+      choice = ApplicationProcess::Choice.new('selection_by_organizer')
 
-	describe 'handling application process' do
-		it 'is valid if its value is allowed' do
-			choice = ApplicationProcess::Choice.new('selection_by_organizer')
+      assert_equal true, choice.valid?
+    end
 
-			assert_equal true, choice.valid?
-		end
+    it 'is not valid if its value is not allowed' do
+      choice = ApplicationProcess::Choice.new('random value')
 
-		it 'is not valid if its value is not allowed' do
-			choice = ApplicationProcess::Choice.new('random value')
-
-			assert_equal false, choice.valid?
-		end
-	end
+      assert_equal false, choice.valid?
+    end
+  end
 end
