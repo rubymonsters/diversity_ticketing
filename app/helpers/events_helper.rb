e4a7f6d2beabbd29197699e95f5a5289d029bf33
ariaru <ariaru@hotmail.de>
2016-09-27 17:20:44 +0200

    Breadcrumbs differ according to user status (admin or not)

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -7,4 +7,12 @@ module EventsHelper
       image_tag "event-default.png"
     end
   end
+
+  def breadcrumb_link_according_to_user_status
+    if current_user && current_user.admin?
+      link_to 'Admin', admin_path
+    else
+      link_to 'Events', events_path
+    end
+  end
 end
