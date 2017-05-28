27ce06ecb509a1e90b2a50310db2e78707eab0b6
Katrin Marschner <katrin.marschner@gmail.com>
2017-05-28 13:17:15 +0200

    send 'deadline passed' emails not for listed events

--- app/services/deadline_passed_mail_service.rb
+++ app/services/deadline_passed_mail_service.rb
@@ -1,6 +1,6 @@
 class DeadlinePassedMailService
   def self.send_deadline_passed_mail
-    Event.deadline_yesterday.each do |event|
+    Event.application_via_diversitytickets.deadline_yesterday.each do |event|
       User.admin.each do |user|
         AdminMailer.passed_event_deadline(event, user.email).deliver_later
       end
--- test/services/deadline_passed_mail_service_test.rb
+++ test/services/deadline_passed_mail_service_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class DeadlinePassedMailServiceTest < ActiveSupport::TestCase
-  describe "sending emails about events with passed deadlines to admins"  do
+  describe "sending emails about events with passed deadlines to admins" do
     it "send emails about right events to the right people" do
       make_event(deadline: 1.days.ago, name: "Valid Event")
       make_event(deadline: 1.week.ago)
@@ -17,6 +17,33 @@ class DeadlinePassedMailServiceTest < ActiveSupport::TestCase
       mail = mails.first
       assert_equal ["good@example.com"], mail.to
       assert mail.subject =~ /Valid Event/
+    end
+
+    it "excludes events that are listings (applications & selection are handled by organizer)" do
+      make_event(
+        deadline: 1.days.ago, name: "Valid Event",
+        application_process: "selection_by_travis"
+      )
+      make_event(
+        deadline: 1.days.ago, name: "Valid Event",
+        application_process: "selection_by_organizer"
+      )
+      make_event(
+        deadline: 1.days.ago, name: "Listed Event",
+        application_process: "application_by_organizer", application_link: "https://myconf.com"
+      )
+      make_admin
+
+      ActionMailer::Base.deliveries.clear
+
+      DeadlinePassedMailService.send_deadline_passed_mail
+
+      mails = ActionMailer::Base.deliveries
+      assert_equal 2, mails.length
+      mail1 = mails.first
+      mail2 = mails.last
+      assert mail1.subject =~ /Valid Event/
+      assert mail2.subject =~ /Valid Event/
     end
   end
 end
