aac28b8194622687f974335da6b10fe59c097be8
Katrin Marschner <katrin.marschner@gmail.com>
2017-08-08 21:27:16 +0200

    send deadline emails only for approved events

--- test/services/deadline_passed_mail_service_test.rb
+++ test/services/deadline_passed_mail_service_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class DeadlinePassedMailServiceTest < ActiveSupport::TestCase
   describe "sending emails about events with passed deadlines to admins" do
     it "send emails about right events to the right people" do
-      make_event(deadline: 1.days.ago, name: "Valid Event")
+      make_event(deadline: 1.days.ago, approved: true, name: "Valid Event")
       make_event(deadline: 1.week.ago)
       make_user(email: "bad@example.com")
       make_admin(email: "good@example.com")
@@ -21,11 +21,11 @@ class DeadlinePassedMailServiceTest < ActiveSupport::TestCase
 
     it "excludes events that are listings (applications & selection are handled by organizer)" do
       make_event(
-        deadline: 1.days.ago, name: "Valid Event",
+        deadline: 1.days.ago, approved: true, name: "Valid Event",
         application_process: "selection_by_travis"
       )
       make_event(
-        deadline: 1.days.ago, name: "Valid Event",
+        deadline: 1.days.ago, approved: true, name: "Valid Event",
         application_process: "selection_by_organizer"
       )
       make_event(
@@ -44,6 +44,26 @@ class DeadlinePassedMailServiceTest < ActiveSupport::TestCase
       mail2 = mails.last
       assert mail1.subject =~ /Valid Event/
       assert mail2.subject =~ /Valid Event/
+    end
+
+    it "excludes events that are not approved" do
+      make_event(
+        deadline: 1.days.ago, approved: true, name: "Valid Event",
+        application_process: "selection_by_travis"
+      )
+      make_event(
+        deadline: 1.days.ago, approved: false, name: "Unapproved Event",
+        application_process: "selection_by_organizer"
+      )
+      make_admin
+
+      ActionMailer::Base.deliveries.clear
+
+      DeadlinePassedMailService.send_deadline_passed_mail
+
+      mails = ActionMailer::Base.deliveries
+      assert_equal 1, mails.length
+      assert mails.first.subject =~ /Valid Event/
     end
   end
 end
