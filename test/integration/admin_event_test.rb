fd4055cacec085b80b8f4adc36afa2a10fc3d031
Katrin Marschner <katrin.marschner@gmail.com>
2017-10-17 20:48:36 +0200

    test unapprove event

--- test/integration/admin_event_test.rb
+++ test/integration/admin_event_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 feature 'Admin Events' do
-  scenario 'Approve event' do
+  scenario 'Approve and unapprove event' do
     click_link 'Admin'
 
     within(approved_events_box) do
@@ -13,19 +13,28 @@ feature 'Admin Events' do
       click_link 'approve'
     end
 
+    within(unapproved_events_box) do
+      refute page.text.include?('The Event')
+    end
+
     within(approved_events_box) do
       assert page.text.include?('The Event')
+      click_link 'approve'
+    end
+
+    within(approved_events_box) do
+      refute page.text.include?('The Event')
     end
 
     within(unapproved_events_box) do
-      refute page.text.include?('The Event')
+      assert page.text.include?('The Event')
     end
   end
 
   before do
     make_admin
     sign_in_as_admin
-    event = make_event(name: "The Event")
+    event = make_event(name: "The Event", approved: false)
     TwitterWorker.expects(:announce_event).with(event).once
   end
 
