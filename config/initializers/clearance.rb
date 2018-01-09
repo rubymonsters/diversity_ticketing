8edaaf87362a6c3b4f9f6682c9a2233a10dd3845
Katrin Marschner <katrin.marschner@gmail.com>
2018-01-09 20:25:18 +0100

    change flash notice to alert on sign in

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,5 @@
 Clearance.configure do |config|
+  config.routes = false
   config.mailer_sender = "info@diversitytickets.org"
   config.cookie_domain = ".diversitytickets.org"
   config.secure_cookie = true
