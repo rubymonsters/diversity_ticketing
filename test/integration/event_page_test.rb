c8937cdbb772fff051e681066e35da62bfc4f71a
mauditecandela <candela.jimenez@gmail.com>
2018-04-05 18:36:47 +0200

    - Removes the setup / before do commands from the integration test in order to check different kinds of events

--- test/integration/event_page_test.rb
+++ test/integration/event_page_test.rb
@@ -1,32 +1,24 @@
 require 'test_helper'
 
 feature 'Event' do
-  before do
+  test 'Events are displayed in the events page' do
     make_user
     sign_in_as_user
-    event = make_event(name: 'The Event', approved: true)
-  end
+    make_event(name: 'The Event', approved: true, logo: 'logo-image')
 
-  scenario 'Events are displayed in the events page' do
     visit root_path
-    click_link 'Events'
 
-    assert page.text.include?('The Event')
-    assert page.body.include?('event-default')
-  end
-
-  before do
-    event = make_event(name: 'The Event', approved: true, logo: 'logo-image')
-  end
-
-  scenario 'Events are displayed in the events page' do
-    visit root_path
     click_link 'Events'
 
     assert page.text.include?('The Event')
     assert page.body.include?('logo-image')
+    assert_not page.body.include?('event-default')
 
-    pp page.body
+    make_event(name: 'The Event 2', approved: true)
+
+    refresh
+
+    assert page.text.include?('The Event 2')
+    assert page.body.include?('event-default')
   end
-
 end
