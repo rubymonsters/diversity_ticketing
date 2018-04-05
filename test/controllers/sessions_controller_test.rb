e2b1e9e3cb87abe2361b5032fb62587f0233d874
mauditecandela <candela.jimenez@gmail.com>
2018-04-05 14:50:48 +0200

    - Adds a new test case for the session creation when the user is properly authenticated Note: After signing in, the user gets redirected to the root. It may be better if it could be redirected to the last page that was checking

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,8 +2,17 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
   describe '#create' do
+    it 'redirects back to the homepage when the user is authenticated' do
+      user = make_user
+
+      post :create, session: { email: user.email, password: user.password }
+
+      assert_redirected_to :root
+    end
+
     it 'shows an unauthorized response when the user is not authenticated' do
       post :create, session: { email: 'email@test.org' }
+
       assert_equal 'Bad email or password.', flash[:alert]
       assert_response :unauthorized
       assert_template 'sessions/new'
