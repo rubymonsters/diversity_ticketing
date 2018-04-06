24a63e25648c2e35fe9ba89bf50f22e0b8149f08
Jessica Grzanna <jgrzanna@me.com>
2018-04-06 12:25:08 +0200

    clean code admin_mailer_test

--- test/mailers/admin_mailer_test.rb
+++ test/mailers/admin_mailer_test.rb
@@ -7,45 +7,36 @@ class AdminMailerTest < ActionMailer::TestCase
   end
 
   test "submitted_event" do
-    # Create the email and store it for further assertions
     email = AdminMailer.submitted_event(@event, @email)
 
-    # Send the email, then test that it got queued
     assert_emails 1 do
       email.deliver_now
     end
 
-    # Test the body of the sent email contains what we expect it to
     assert_equal ['info@diversitytickets.org'], email.from
     assert_equal [@email], email.to
     assert_equal 'A new event has been submitted.', email.subject
   end
 
   test "upcoming_event_deadline" do
-    # Create the email and store it for further assertions
     email = AdminMailer.upcoming_event_deadline(@event, @email)
 
-    # Send the email, then test that it got queued
     assert_emails 1 do
       email.deliver_now
     end
 
-    # Test the body of the sent email contains what we expect it to
     assert_equal ['info@diversitytickets.org'], email.from
     assert_equal [@email], email.to
     assert_equal "#{@event.name} deadline in two days.", email.subject
   end
 
   test "passed_event_deadline" do
-    # Create the email and store it for further assertions
     email = AdminMailer.passed_event_deadline(@event, @email)
 
-    # Send the email, then test that it got queued
     assert_emails 1 do
       email.deliver_now
     end
 
-    # Test the body of the sent email contains what we expect it to
     assert_equal ['info@diversitytickets.org'], email.from
     assert_equal [@email], email.to
     assert_equal "#{@event.name} deadline yesterday.", email.subject
