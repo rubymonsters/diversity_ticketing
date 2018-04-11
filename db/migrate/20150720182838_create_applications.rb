2b143512ca819bb9cb15ef4eb72e8518f7b895ab
mauditecandela <candela.jimenez@gmail.com>
2018-04-11 12:34:24 +0200

    Adds the rails version to the db migration - it was failing in travis

--- db/migrate/20150720182838_create_applications.rb
+++ db/migrate/20150720182838_create_applications.rb
@@ -1,4 +1,4 @@
-class CreateApplications < ActiveRecord::Migration
+class CreateApplications < ActiveRecord::Migration[5.1]
   def change
     create_table :applications do |t|
       t.string :name
