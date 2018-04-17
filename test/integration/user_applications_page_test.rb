9ad287cf4f333ac88f5ba188470139db9c9e46db
Jessica Grzanna <jgrzanna@me.com>
2018-04-17 12:40:32 +0200

    rename applicants_user_page_test to user_applications_page_test and edit test content

--- test/integration/user_applications_page_test.rb
+++ test/integration/user_applications_page_test.rb
@@ -1,20 +1,18 @@
 require 'test_helper'
 
-feature 'Applicants User Account' do
+feature 'User Applications Page' do
   def setup
     @applicant = make_user
     @event = make_event(name: 'Applicants event')
-    @application = make_application(applicant_id: @applicant.id, event_id: @event.id)
+    @application = make_application(event: @event, applicant_id: @applicant.id)
   end
 
-  test 'shows a Your applications section where the applications are displayed' do
+  test 'shows a section to Your applications where the users applications are displayed' do
     sign_in_as(@applicant)
 
     visit root_path
 
     click_link 'Your applications'
-
-    visit events_path
 
     assert page.text.include?('Applicants event')
     assert_select 'h2', 'Your applications'
