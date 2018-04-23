ff12d99002e05f3e3aa52e41d8649fdc41981265
Jessica Grzanna <jgrzanna@me.com>
2018-04-23 15:03:01 +0200

    adds tests for showing delete-button of applications for admins and not for applicants

--- test/integration/application_page_test.rb
+++ test/integration/application_page_test.rb
@@ -20,4 +20,24 @@ feature 'Application' do
     assert page.has_content?('Your Application')
     assert_equal 2, page.all("a[href='/users/#{@user.id}/applications']").count
   end
+
+  test 'shows link to delete this application if user is an admin' do
+    sign_in_as_admin
+    event = make_event(name: 'The Event', approved: true)
+    application = make_application(event, applicant_id: @user.id)
+
+    visit event_application_path(event.id, application.id)
+
+    assert page.has_content?('Delete this application')
+  end
+
+  test 'does not show link to delete this application if you are an applicant' do
+    sign_in_as_user
+    event = make_event(name: 'The Event', approved: true)
+    application = make_application(event, applicant_id: @user.id)
+
+    visit event_application_path(event.id, application.id)
+
+    assert_not page.has_content?('Delete this application')
+  end
 end
