c645a326f8e875fa14c1abdd4092c4201c91273b
mauditecandela <candela.jimenez@gmail.com>
2018-04-23 18:03:08 +0200

    Adds new different_data method only as a temporary solution for the checkbox

--- app/models/application.rb
+++ app/models/application.rb
@@ -5,4 +5,7 @@ class Application < ApplicationRecord
   validates :email, presence: true, confirmation: true, format: { with: /.+@.+\..+/ }
   validates :email_confirmation, presence: true
   validates :terms_and_conditions, acceptance: true, allow_nil: false
+
+  def different_data
+  end
 end
