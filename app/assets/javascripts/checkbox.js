64543880d377089b921bebdf1ac5c91fed37357f
mauditecandela <candela.jimenez@gmail.com>
2018-04-24 12:18:12 +0200

    - Hides also the already filled information if the user clicks on the checkbox

--- app/assets/javascripts/checkbox.js
+++ app/assets/javascripts/checkbox.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   let checkbox = document.getElementById('different_data');
   let form = document.getElementById('form_different_data');
+  let user_information = document.getElementById('user_profile_information');
   checkbox.onchange = function() {
      if(this.checked) {
        form.className = "form_field";
+       user_information.className = "form_field--hidden";
      } else {
        form.className = "form_field--hidden";
+       user_information.className = "form_field";
      };
    };
 });
