d4b4c5432454bdf0fa04eb42b42e3bb7d9758260
mauditecandela <candela.jimenez@gmail.com>
2018-04-24 12:38:38 +0200

    Adds a test for the breadcrumb if the user is an admin

--- test/integration/application_page_test.rb
+++ test/integration/application_page_test.rb
@@ -20,4 +20,20 @@ feature 'Application' do
     assert page.has_content?('Your Application')
     assert_equal 2, page.all("a[href='/users/#{@user.id}/applications']").count
   end
+
+  test 'shows Admin link in the breadcrumb if the user is an admin' do
+    sign_in_as_admin
+    event = make_event(name: 'The Event', approved: true)
+    application = make_application(event, applicant_id: @admin.id)
+
+    visit user_applications_path(@admin.id)
+
+    click_link 'Your Application'
+
+    assert_equal current_path, event_application_path(event.id,application.id)
+
+    assert page.has_content?('Your Application')
+    assert_equal 1, page.all("a[href='/users/#{@admin.id}/applications']").count
+    assert_equal 2, page.all("a[href='/admin']").count
+  end
 end
