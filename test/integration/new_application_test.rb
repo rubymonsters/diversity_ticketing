c2aa1ae73717bb6d9ffaf6d45e82b087490d2479
mauditecandela <candela.jimenez@gmail.com>
2018-04-24 15:46:09 +0200

    Adds two new cases to the integration tests: 1. checks if an error is raised when information is missing, 2. checks if the information from the formular takes precedence even if the user is signed in

--- test/integration/new_application_test.rb
+++ test/integration/new_application_test.rb
@@ -39,4 +39,35 @@ feature 'New Application' do
 
     assert page.text.include?("You have successfully applied for #{@event.name}")
   end
+
+  test 'raises an error if a field was not included properly' do
+    sign_in_as_user
+
+    visit event_path(@event.id)
+
+    click_button "Apply"
+
+    click_button "Submit Application"
+
+    assert page.text.include?("3 errors stopped this application from being saved")
+  end
+
+  test 'keeps information introduced in the application event if the user is signed in' do
+    sign_in_as_user
+
+    visit event_path(@event.id)
+
+    click_button "Apply"
+
+    page.fill_in 'application_name', with: "New name"
+    page.fill_in 'application_attendee_info_1', with: 'I want to learn how to code'
+    page.fill_in 'application_attendee_info_2', with: 'I am an underrepresented minority'
+    page.check 'application[terms_and_conditions]'
+
+    click_button "Submit Application"
+
+    assert page.text.include?("You have successfully applied for #{@event.name}")
+
+    assert_equal Application.find_by(applicant_id: @user.id).name, "New name"
+  end
 end
