71e9763a185727a3f4bff307693f034980a46db6
Jessica Grzanna <jgrzanna@me.com>
2018-04-25 12:46:04 +0200

    refactoring application_page_test

--- test/integration/application_page_test.rb
+++ test/integration/application_page_test.rb
@@ -4,18 +4,23 @@ feature 'Application' do
   def setup
     @user = make_user
     @admin = make_admin
+    @event = make_event(name: 'The Event', approved: true)
+    @application = make_application(
+                  @event,
+                  applicant_id: @user.id,
+                  attendee_info_1: 'I would like to learn Ruby',
+                  attendee_info_2: 'I can not afford the ticket'
+                )
   end
 
   test 'shows Your applications in the breadcrumb if the user is not an admin' do
     sign_in_as_user
-    event = make_event(name: 'The Event', approved: true)
-    application = make_application(event, applicant_id: @user.id)
 
     visit user_applications_path(@user.id)
 
     click_link 'Your Application'
 
-    assert_equal current_path, event_application_path(event.id,application.id)
+    assert_equal current_path, event_application_path(@event.id,@application.id)
 
     assert page.has_content?('Your Application')
     assert_equal 2, page.all("a[href='/users/#{@user.id}/applications']").count
@@ -23,35 +28,24 @@ feature 'Application' do
 
   test 'shows link to delete this application if user is an admin' do
     sign_in_as_admin
-    event = make_event(name: 'The Event', approved: true)
-    application = make_application(event, applicant_id: @user.id)
 
-    visit event_application_path(event.id, application.id)
+    visit event_application_path(@event.id, @application.id)
 
     assert page.has_content?('Delete this application')
   end
 
-  test 'does not show link to delete this application if you are an applicant' do
+  test 'does not show link to delete this application if user is an applicant' do
     sign_in_as_user
-    event = make_event(name: 'The Event', approved: true)
-    application = make_application(event, applicant_id: @user.id)
 
-    visit event_application_path(event.id, application.id)
+    visit event_application_path(@event.id, @application.id)
 
     assert_not page.has_content?('Delete this application')
   end
 
   test 'does show the correct content of the application' do
     sign_in_as_user
-    event = make_event(name: 'The Event', approved: true)
-    application = make_application(
-                  event,
-                  applicant_id: @user.id,
-                  attendee_info_1: 'I would like to learn Ruby',
-                  attendee_info_2: 'I can not afford the ticket'
-                )
 
-    visit event_application_path(event.id, application.id)
+    visit event_application_path(@event.id, @application.id)
 
     assert page.has_content?('I would like to learn Ruby')
     assert page.has_content?('I can not afford the ticket')
