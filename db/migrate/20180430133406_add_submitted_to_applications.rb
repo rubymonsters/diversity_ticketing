eb3382a3bee66f975d35d774284d0bb04af29600
mauditecandela <candela.jimenez@gmail.com>
2018-04-30 15:53:23 +0200

    Adds a default for the applications submitted value

--- db/migrate/20180430133406_add_submitted_to_applications.rb
+++ db/migrate/20180430133406_add_submitted_to_applications.rb
@@ -1,5 +1,5 @@
 class AddSubmittedToApplications < ActiveRecord::Migration[5.2]
   def change
-    add_column :applications, :submitted, :boolean
+    add_column :applications, :submitted, :boolean, default: false, null: false
   end
 end
