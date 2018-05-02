a20fe3f2235874bbb77b3ab36677cfa5d6afd2b9
mauditecandela <candela.jimenez@gmail.com>
2018-05-02 12:54:26 +0200

    Adds integration test in order to implement the drafts section in Your applications

--- test/integration/user_applications_page_test.rb
+++ test/integration/user_applications_page_test.rb
@@ -4,10 +4,11 @@ feature 'User Applications Page' do
   def setup
     @user = make_user
     @event = make_event(name: 'Applicants event')
-    @application = make_application(@event, applicant_id: @user.id)
+    @application = make_application(@event, applicant_id: @user.id, submitted: true)
+    @draft = make_application(@event, applicant_id: @user.id, submitted: false)
   end
 
-  test 'shows a section in Your Applications where the users applications are displayed' do
+  test 'shows a section in Your Applications where the users applications and drafts are displayed' do
     sign_in_as_user
 
     visit root_path
@@ -16,9 +17,11 @@ feature 'User Applications Page' do
 
     assert_equal current_path, user_applications_path(@user.id)
 
-    assert page.find_link('Applicants event')
-    assert_equal event_path(@event.id), page.find_link('Applicants event')[:href]
-    assert page.find_link('Your Application')
+    page.assert_selector('h2', text: "Submitted")
+    page.assert_selector('h2', text: "Drafts")
+    page.assert_selector('a', text: @event.name, count: 2)
+    assert_equal event_path(@event.id), page.find_link(@event.name).first[:href]
     assert_equal event_application_path(@event.id, @application.id), page.find_link('Your Application')[:href]
+    assert_equal event_application_path(@event.id, @draft.id), page.find_link('Your Draft')[:href]
   end
 end
