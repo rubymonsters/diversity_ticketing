aa7a71e68d832e4ea5e386d04f4f3907ff42cf0d
Jessica Grzanna <jgrzanna@me.com>
2018-05-02 11:30:19 +0200

    adds test to admin_event_details integration test to check:  - that details of submitted applications are shown  - that details of draft applications are not shown

--- test/integration/admin_event_details_test.rb
+++ test/integration/admin_event_details_test.rb
@@ -5,9 +5,9 @@ feature 'Admin Event Details' do
   def setup
     @admin = make_admin
     @event = make_event
-    @application_1 = make_application(@event, submitted: true)
-    @application_2 = make_application(@event, submitted: true)
-    @application_draft = make_application(@event, submitted: false)
+    @application_1 = make_application(@event, name: 'Peter', submitted: true)
+    @application_2 = make_application(@event, name: 'Paul', submitted: true)
+    @application_draft = make_application(@event, name: 'Mary', submitted: false)
   end
 
   test 'shows number of submitted applications' do
@@ -24,5 +24,23 @@ feature 'Admin Event Details' do
     visit admin_event_path(@event.id)
 
     assert page.has_content?('1 draft currently')
+  end
+
+  test 'does not show draft applications details on event details page for admin' do
+    sign_in_as_admin
+
+    visit admin_event_path(@event.id)
+
+    assert_not page.has_content?('Mary')
+
+  end
+
+  test 'shows submitted applications on event details page for admin' do
+    sign_in_as_admin
+
+    visit admin_event_path(@event.id)
+
+    assert page.has_content?('Peter')
+    assert page.has_content?('Paul')
   end
 end
