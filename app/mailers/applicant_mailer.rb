9b1a32b5be9cc9dbc096cd8ae1685c10eb54a0a9
mauditecandela <candela.jimenez@gmail.com>
2018-05-07 18:42:07 +0200

    Creates a new deadline reminder for applicants

--- app/mailers/applicant_mailer.rb
+++ app/mailers/applicant_mailer.rb
@@ -5,4 +5,9 @@ class ApplicantMailer < ApplicationMailer
     mail(to: @application.email, subject: "Your application for #{@application.event.name}.")
 	end
 
+	def deadline_reminder(application)
+		@application = application
+		mail(to: @application.email, subject: "#{@application.event.name} deadline in two days.")
+	end
+
 end
