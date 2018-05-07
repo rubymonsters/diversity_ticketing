a57510c06052d6a3cc99281e9d389ca1fffd2bcf
mauditecandela <candela.jimenez@gmail.com>
2018-05-07 12:22:09 +0200

    - Overrides the http referer in the session creation by the params[:referer] one - Uses redirect_back instead of redirect_to in the session creation so we can use a fallback location - This way, if the user is changing their user and  the referer is private page from the previous user, it will be redirected to the root

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,11 @@ class SessionsController < Clearance::SessionsController
 
   def create
     @user = authenticate(params)
+    request.env['HTTP_REFERER'] = params[:referer]
 
     sign_in(@user) do |status|
       if status.success?
-        redirect_to(params[:referer])
+        redirect_back(fallback_location: root_path)
       else
         flash[:alert] = status.failure_message
         render template: "sessions/new", status: :unauthorized
