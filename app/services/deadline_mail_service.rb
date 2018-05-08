22adb718f8cef20b89c5875d4033aa9eb9660b3e
Jessica Grzanna <jgrzanna@me.com>
2018-05-08 12:09:40 +0200

    adds .deliver_later to deadline_mail_service "send_deadline_reminder_applicants"

--- app/services/deadline_mail_service.rb
+++ app/services/deadline_mail_service.rb
@@ -10,7 +10,7 @@ class DeadlineMailService
   def self.send_deadline_reminder_applicants
     Event.approved.deadline_in_two_days.each do |event|
       event.applications.where(submitted: false).each do |application|
-        ApplicantMailer.deadline_reminder(application)
+        ApplicantMailer.deadline_reminder(application).deliver_later
       end
     end
   end
