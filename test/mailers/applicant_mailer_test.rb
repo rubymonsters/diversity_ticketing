9b4ea29edcaedddc2ba0932cf80445d998117c46
Jessica Grzanna <jgrzanna@me.com>
2018-05-08 12:03:42 +0200

    - edits applicant_mailer_test to not check for email delivery - creates service_tests for send_deadline_reminder_applicants (tests service behaviour to send all applicants a deadline-reminder that have saved drafts for an upcoming event)

--- test/mailers/applicant_mailer_test.rb
+++ test/mailers/applicant_mailer_test.rb
@@ -23,10 +23,6 @@ class ApplicantMailerTest < ActionMailer::TestCase
     @event.update_attributes(deadline: 2.days.from_now)
     email = ApplicantMailer.deadline_reminder(@draft)
 
-    assert_emails 1 do
-      email.deliver_now
-    end
-
     assert_equal ['info@diversitytickets.org'], email.from
     assert_equal [@draft.email], email.to
     assert_equal "#{@draft.event.name} deadline in two days.", email.subject
