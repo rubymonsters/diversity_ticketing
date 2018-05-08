1e848c368b8b79c51204aa56f61bc3269ffadfbc
Jessica Grzanna <jgrzanna@me.com>
2018-05-08 17:25:44 +0200

    syntax refactoring for consistency

--- config/routes.rb
+++ config/routes.rb
@@ -21,12 +21,12 @@ Rails.application.routes.draw do
 
   resources :tags, only: [:index, :destroy, :create]
 
-  get '/sign_in', to: 'clearance/sessions#new', as: 'sign_in'
-  delete '/sign_out', to: 'clearance/sessions#destroy', as: 'sign_out'
-  get '/sign_up', to: 'clearance/users#new', as: 'sign_up'
+  get '/sign_in', to: 'clearance/sessions#new', as: :sign_in
+  delete '/sign_out', to: 'clearance/sessions#destroy', as: :sign_out
+  get '/sign_up', to: 'clearance/users#new', as: :sign_up
   get '/past_events', to: 'events#index_past', as: :past_events
   get '/admin', to: 'admin_events#index'
-  delete '/events/:event_id/application/:id', to: 'applications#admin_destroy', as: 'admin_event_application'
+  delete '/events/:event_id/application/:id', to: 'applications#admin_destroy', as: :admin_event_application
   get '/events/:id/admin', to: 'admin_events#show', as: :event_admin
   post '/events/:event_id/applications/:id', to: 'applications#submit', as: :submit_event_application
   post '/events/preview', to: 'events#preview', as: :event_preview
