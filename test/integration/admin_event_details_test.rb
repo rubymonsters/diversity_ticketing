fbf35a11f8ad98eeaf578279f32a0468bbf1f2c2
Jessica Grzanna <jgrzanna@me.com>
2018-05-09 13:40:32 +0200

    adds tests to check: - lists of list pending, approved and rejected applications is shown - clicking on the approved link changes the application state to approved and flashes a notice - clicking on the rejected link changes the application state to rejected and flashes an info - clicking on the undo link changes the application state back to pending and flashes an info - lists show application status counts

--- test/integration/admin_event_details_test.rb
+++ test/integration/admin_event_details_test.rb
@@ -5,8 +5,9 @@ feature 'Admin Event Details' do
   def setup
     @admin = make_admin
     @event = make_event
-    @application_1 = make_application(@event, name: 'Peter')
-    @application_2 = make_application(@event, name: 'Paul')
+    @application_1 = make_application(@event, name: 'Peter', status: 'pending')
+    @application_2 = make_application(@event, name: 'Paul', status: 'pending')
+    @application_3 = make_application(@event, name: 'Lara', status: 'approved')
     @application_draft = make_draft(@event, name: 'Mary')
   end
 
@@ -44,12 +45,84 @@ feature 'Admin Event Details' do
     assert page.has_content?('Paul')
   end
 
-  test 'shows icons for approve and reject' do
+  test 'shows icons for approve and reject links' do
     sign_in_as_admin
 
     visit admin_event_path(@event.id)
 
-    page.assert_selector('i.fas fa-check')
-    page.assert_selector('i.fas fa-times')
+    page.assert_selector('i.fa-check')
+    page.assert_selector('i.fa-times')
+  end
+
+  test 'shows a list of pending applications' do
+    sign_in_as_admin
+
+    visit admin_event_path(@event.id)
+
+    assert page.has_content?("Pending (2)")
+  end
+
+  test 'shows a list of approved applications' do
+    sign_in_as_admin
+
+    visit admin_event_path(@event.id)
+
+    assert page.has_content?("Approved (1)")
+  end
+
+  test 'shows a list of rejected applications' do
+    sign_in_as_admin
+
+    visit admin_event_path(@event.id)
+
+    assert page.has_content?("Rejected (0)")
+  end
+
+  test 'changes application status after click on approved link' do
+    sign_in_as_admin
+
+    visit admin_event_path(@event.id)
+
+    assert page.has_content?("Approved (1)")
+    assert page.has_content?("Pending (2)")
+
+    click_link('.fa-check').last
+
+    assert_equal "Paul's application has been approved!", flash[:notice]
+
+    assert page.has_content?("Approved (2)")
+    assert page.has_content?("Pending (1)")
+  end
+
+  test 'changes application status after click on reject link' do
+    sign_in_as_admin
+
+    visit admin_event_path(@event.id)
+
+    assert page.has_content?("Pending (2)")
+    assert page.has_content?("Rejected (0)")
+
+    click_link('.fa-times').first
+
+    assert_equal "Peter's application has been rejected", flash[:info]
+
+    assert page.has_content?("Pending (1)")
+    assert page.has_content?("Rejected (1)")
+  end
+
+  test 'changes application status back to pending after clicking on undo link' do
+    sign_in_as_admin
+
+    visit admin_event_path(@event.id)
+
+    assert page.has_content?("Approved (1)")
+    assert page.has_content?("Pending (2)")
+
+    click_link('Undo')
+
+    assert_equal "Lara's application has been changed to pending", flash[:info]
+
+    assert page.has_content?("Approved (0)")
+    assert page.has_content?("Pending (3)")
   end
 end
