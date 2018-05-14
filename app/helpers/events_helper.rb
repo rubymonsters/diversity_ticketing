cd590e881acf1d0296a29f84ab76944d5bcc3331
mauditecandela <candela.jimenez@gmail.com>
2018-05-14 11:07:15 +0200

    Fixes the breadcrumb after breaking it in previous commit

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -9,13 +9,17 @@ module EventsHelper
   end
 
   def breadcrumb_link_according_to_user_status
-    if @event && @event.organizer_id == current_user.id
-      link_to 'Your Events', user_path(current_user)
-    elsif current_user.admin?
-      link_to 'Admin', admin_path
+    if current_user
+      if @event && @event.organizer_id == current_user.id
+        link_to 'Your Events', user_path(current_user)
+      elsif current_user.admin?
+        link_to 'Admin', admin_path
+      else
+        link_to 'Events', events_path
+      end
     else
       link_to 'Events', events_path
     end
   end
-  
+
 end
