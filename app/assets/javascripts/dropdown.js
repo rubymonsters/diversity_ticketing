d65409bd97e53f7a0ba487d69f4fc7dfb3fb90af
Jessica Grzanna <jgrzanna@me.com>
2018-05-14 11:16:24 +0200

    makes menu disappear when you click somewhere else on the page

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
   $( "#dropdown-btn" ).click(function() {
     $(".dropdown-content").toggle();
   });
+
+  //makes menu disappear when you click somewhere else on the page:
+  $(document).click(function() {
+    if ($(".dropdown-content").is( ":visible" )) {
+      if ($(event.target).is('#dropdown-btn')) {
+        return;
+      }
+      else {
+        $(".dropdown-content").toggle();
+      }
+    }
+  });
 });
