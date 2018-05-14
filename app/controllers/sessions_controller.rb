50aa635bf9c51be4532f52f11f5b7d0ed32aa88b
Jessica Grzanna <jgrzanna@me.com>
2018-05-14 14:51:09 +0200

    Upon login: Detecs all users with 'old' passwords that have encrypted_password: 'x' and refers them to the reset-password page with a flash-info.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,19 @@
 class SessionsController < Clearance::SessionsController
 
   def create
-    @user = authenticate(params)
+    if User.find_by(email: params[:session][:email]).encrypted_password === 'x'
+      redirect_to  new_password_path, flash: { :info => "Due to a security update from our side we kindly ask you to reset your password." }
 
-    sign_in(@user) do |status|
-      if status.success?
-        redirect_to params[:referer]
-      else
-        flash[:alert] = status.failure_message
-        render template: "sessions/new", status: :unauthorized
+    else @user = authenticate(params)
+
+      sign_in(@user) do |status|
+
+        if status.success?
+            redirect_to params[:referer]
+        else
+          flash[:alert] = status.failure_message
+          render template: "sessions/new", status: :unauthorized
+        end
       end
     end
   end
