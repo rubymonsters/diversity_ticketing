77c39f03e7f8eceacb071bdce56307c22f4bd406
mauditecandela <candela.jimenez@gmail.com>
2018-06-04 16:28:48 +0200

    Adds new test for user notifications mailer

---
+++ test/mailers/user_notifications_mailer_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class UserNotificationsMailerTest < ActionMailer::TestCase
+  def setup
+    @event = make_event
+    @user = make_user
+  end
+
+  test "submitted_event" do
+    email = UserNotificationsMailer.submitted_event(@event, @user)
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['info@diversitytickets.org'], email.from
+    assert_equal [@user.email], email.to
+    assert_equal 'A new event in your country!', email.subject
+  end
+
+end
