c0c3947145737fe194cee4f8d75b3110b70d0265
mauditecandela <candela.jimenez@gmail.com>
2018-06-04 15:10:02 +0200

    Adds the emails delivery after the Admin has approved the new event

--- app/controllers/admin_events_controller.rb
+++ app/controllers/admin_events_controller.rb
@@ -38,6 +38,9 @@ class AdminEventsController < ApplicationController
     @event.toggle(:approved)
     @event.save!
     if @event.approved?
+      User.where(country: @event.country).where(country_email_notifications: true).each do |user|
+        UserNotificationsMailer.submitted_event(@event, user)
+      end
       redirect_to admin_url, notice: "#{@event.name} has been approved!"
     else
       redirect_to admin_url, notice: "#{@event.name} has been unapproved!"
