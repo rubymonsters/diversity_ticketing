9b7ec29f799ac7d76e8f1678a3a43d4e660be5a9
mauditecandela <candela.jimenez@gmail.com>
2018-06-04 15:24:47 +0200

    Adds new "Country" column to the users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_094835) do
+ActiveRecord::Schema.define(version: 2018_06_04_132133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,8 +102,8 @@ ActiveRecord::Schema.define(version: 2018_06_04_094835) do
     t.string "remember_token", limit: 128, null: false
     t.boolean "admin", default: false, null: false
     t.string "name"
-    t.string "country"
     t.boolean "country_email_notifications", default: false
+    t.string "country"
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
