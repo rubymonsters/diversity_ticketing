0c5ce0f83acf6d6d52a5acea63eff0ea9ea42115
mauditecandela <candela.jimenez@gmail.com>
2018-06-13 10:50:00 +0200

    Moves tests to the integration test they belong to Adds new tests for delete button

--- test/integration/event_page_test.rb
+++ test/integration/event_page_test.rb
@@ -4,59 +4,25 @@ feature 'Event' do
   def setup
     @user = make_user
     @admin = make_admin
+    @event = make_event(organizer_id: @user.id, approved: true)
   end
 
-  test 'shows a logo image if the event has a logo' do
+  test 'does not show a Delete button if the Event is still open' do
     sign_in_as_user
-    make_event(name: 'The Event', approved: true, logo: 'logo-image')
-
-    visit events_path
-
-    assert page.text.include?('The Event')
-    assert_match /logo-image/, page.body
-    assert_no_match /event-default-.*\.png/, page.body
+    visit event_path(@event.id)
+    assert_not page.has_link?("Delete")
   end
 
-  test 'shows a default image if the event does not have a logo' do
+  test 'shows a Delete button if the Event has passed and the user is the organizer' do
     sign_in_as_user
-    make_event(name: 'The Event', approved: true)
-
-    visit events_path
-
-    assert page.text.include?('The Event')
-    assert_match /event-default-.*\.png/, page.body
+    @event.update_attributes(start_date: 1.week.ago, end_date: 1.day.ago, deadline: 1.week.ago)
+    visit event_path(@event.id)
+    assert page.has_link?("Delete")
   end
 
-  test 'shows Your events in the breadcrumb if the user is an organizer' do
-    sign_in_as_user
-    make_event(name: 'The Event', approved: true, organizer_id: @user.id)
-
-    visit events_path
-
-    click_link 'The Event'
-
-    assert page.text.include?('Your Events')
-  end
-
-  test 'shows Events in the breadcrumb if the user is not an organizer' do
-    sign_in_as_user
-    make_event(name: 'The Event', approved: true)
-
-    visit events_path
-
-    click_link 'The Event'
-
-    assert page.text.include?('Events')
-  end
-
-  test 'shows Admin in the breadcrumb if the user is an Admin' do
-    sign_in_as_admin
-    make_event(name: 'The Event', approved: true, organizer_id: @user.id)
-
-    visit events_path
-
-    click_link 'The Event'
-
-    assert page.text.include?('Admin')
+  test 'does not show a Delete button if the user is not organizer' do
+    @event.update_attributes(start_date: 1.week.ago, end_date: 1.day.ago, deadline: 1.week.ago)
+    visit event_path(@event.id)
+    assert_not page.has_link?("Delete")
   end
 end
--- test/integration/events_overview_test.rb
+++ test/integration/events_overview_test.rb
@@ -3,10 +3,12 @@ require 'test_helper'
 feature 'Event Overview' do
   def setup
     @user = make_user
-    @event = make_event(approved: true)
+    @admin = make_admin
   end
 
   test 'shows a link to Apply if the user did not apply or create a draft for the event' do
+    @event = make_event(approved: true)
+
     sign_in_as_user
 
     visit events_path
@@ -17,6 +19,7 @@ feature 'Event Overview' do
   end
 
   test 'shows a link to Your Draft if the user already created a draft for the event' do
+    @event = make_event(approved: true)
     application = make_draft(@event, applicant_id: @user.id)
     sign_in_as_user
 
@@ -30,6 +33,7 @@ feature 'Event Overview' do
   end
 
   test 'shows a link to Your Application if the user already submitted an application for the event' do
+    @event = make_event(approved: true)    
     application = make_application(@event, applicant_id: @user.id)
     sign_in_as_user
 
@@ -41,5 +45,59 @@ feature 'Event Overview' do
     click_button("Your Application")
 
     assert_equal event_application_path(@event.id, application.id), current_path
+  end
+
+  test 'shows a logo image if the event has a logo' do
+    sign_in_as_user
+    make_event(name: 'The Event', approved: true, logo: 'logo-image')
+
+    visit events_path
+
+    assert page.text.include?('The Event')
+    assert_match /logo-image/, page.body
+    assert_no_match /event-default-.*\.png/, page.body
+  end
+
+  test 'shows a default image if the event does not have a logo' do
+    sign_in_as_user
+    make_event(name: 'The Event', approved: true)
+
+    visit events_path
+
+    assert page.text.include?('The Event')
+    assert_match /event-default-.*\.png/, page.body
+  end
+
+  test 'shows Your events in the breadcrumb if the user is an organizer' do
+    sign_in_as_user
+    make_event(name: 'The Event', approved: true, organizer_id: @user.id)
+
+    visit events_path
+
+    click_link 'The Event'
+
+    assert page.text.include?('Your Events')
+  end
+
+  test 'shows Events in the breadcrumb if the user is not an organizer' do
+    sign_in_as_user
+    make_event(name: 'The Event', approved: true)
+
+    visit events_path
+
+    click_link 'The Event'
+
+    assert page.text.include?('Events')
+  end
+
+  test 'shows Admin in the breadcrumb if the user is an Admin' do
+    sign_in_as_admin
+    make_event(name: 'The Event', approved: true, organizer_id: @user.id)
+
+    visit events_path
+
+    click_link 'The Event'
+
+    assert page.text.include?('Admin')
   end
 end
