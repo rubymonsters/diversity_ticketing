9caf60c2738273ab9c52ad677ffc5e1307e59349
mauditecandela <candela.jimenez@gmail.com>
2018-06-25 18:14:02 +0200

    Sets the tweet as published true if it was indeed published in twitter

--- app/workers/twitter_worker.rb
+++ app/workers/twitter_worker.rb
@@ -5,7 +5,7 @@ class TwitterWorker
   def self.announce_event(event)
     event_url = routes.event_url(event)
     perform_async(event.name, format_date(event.deadline), event_url, event.twitter_handle)
-    Tweet.create(event_id: event.id)
+    Tweet.create(event_id: event.id, published: true)
   end
 
   def self.routes
