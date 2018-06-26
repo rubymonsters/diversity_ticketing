57e687036dcde4b593c9cb4dceb4a2aea449ae0a
Jessica Grzanna <jgrzanna@me.com>
2018-06-26 12:35:13 +0200

    refactors past_event_date_services to delete data properly

--- app/services/past_event_date_service.rb
+++ app/services/past_event_date_service.rb
@@ -2,10 +2,12 @@ class PastEventDateService
   # Looks for all events in the past week to overwrite applications for security
   # just in case the task does not always run properly:
   def self.delete_application_data_after_event
-    Event.approved.where('end_date < ? AND end_date > ?', Time.zone.now, 1.week.ago).delete_application_data
+    events = Event.approved.where('end_date < ? AND end_date > ?', Time.zone.now, 1.week.ago)
+    events.map { |e| e.delete_application_data }
   end
 
   def self.delete_all_past_events_application_data
-    Event.approved.past.delete_application_data
+    events = Event.approved.past
+    events.map { |e| e.delete_application_data }
   end
 end
