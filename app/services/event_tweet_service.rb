26dc6adad5d6d18aebbcecf31c5b92e40dc30bf4
mauditecandela <candela.jimenez@gmail.com>
2018-06-26 15:23:34 +0200

    Fixes service to actually skip tweets that were selected not to be tweeted

--- app/services/event_tweet_service.rb
+++ app/services/event_tweet_service.rb
@@ -1,16 +1,19 @@
 class EventTweetService
   def self.tweet_approved_event
-    remaining_events = Event.open.approved.order("deadline ASC")
+    events = Event.open.approved.order("deadline ASC").ids
+    unpublished = Tweet.where(published: false).pluck(:event_id)
+    remaining_events = events - unpublished
+
     return if remaining_events.empty?
 
     if Tweet.last
-      return if Tweet.last.event_id == remaining_events.last.id
-      last_tweet = Event.find_by(id: Tweet.last.event_id)
+      return if Tweet.where(published: true).last == remaining_events.last
+      last_tweet = Event.find_by(id: Tweet.where(published: true).last.event_id).id
       event_index = remaining_events.index(last_tweet) + 1
     else
       event_index = 0
     end
-
-    TwitterWorker.announce_event(remaining_events[event_index])
+    event_to_announce = Event.find(remaining_events[event_index])
+    TwitterWorker.announce_event(event_to_announce)
   end
 end
