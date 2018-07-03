4696c498a1469f65466883aa57c68d0d83481d53
mauditecandela <candela.jimenez@gmail.com>
2018-07-03 14:13:16 +0200

    Creates a new admin_applications_controller to include those actions taht are only for admins

---
+++ app/controllers/admin_applications_controller.rb
@@ -0,0 +1,44 @@
+class AdminApplicationsController < ApplicationController
+  before_action :require_admin
+  before_action :get_application
+    before_action :skip_validation, only: [:approve, :reject, :revert]
+
+  def approve
+    @application.update_attributes(status: "approved")
+    redirect_to admin_event_path(@application.event_id), notice: "#{@application.name}'s application has been approved!"
+  end
+
+  def reject
+    @application.update_attributes(status: "rejected")
+    redirect_to admin_event_path(@application.event_id), flash: { :info => "#{@application.name}'s application has been rejected" }
+  end
+
+  def revert
+    @application.update_attributes(status: "pending")
+    redirect_to admin_event_path(@application.event_id), flash: { :info => "#{@application.name}'s application has been changed to pending" }
+  end
+
+  def destroy
+    @application.destroy
+    redirect_to event_admin_path(@event.id)
+  end
+
+  private
+  def application_params
+    params.require(:application).permit(:name, :email, :email_confirmation, :attendee_info_1,
+    :attendee_info_2, :visa_needed, :terms_and_conditions, :applicant_id)
+  end
+
+  def get_application
+    get_event
+    @application = @event.applications.find(params[:id])
+  end
+
+  def get_event
+    @event = Event.find(params[:event_id])
+  end
+
+  def skip_validation
+    @application.skip_validation = true
+  end
+end
