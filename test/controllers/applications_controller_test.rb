77b0158ed8aa82d89c3899a0dc18fd39e26b1d97
mauditecandela <candela.jimenez@gmail.com>
2018-07-04 14:21:37 +0200

    Adds new controller tests to get 100% coverage on the refactored controllers

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -1,87 +1,6 @@
 require 'test_helper'
 
 class ApplicationsControllerTest < ActionController::TestCase
-  describe '#show' do
-    it 'does not show application to visitors' do
-      event = make_event
-      application = make_application(event)
-
-      get :show, params: { event_id: event.id, id: application.id }
-
-      assert_redirected_to sign_in_path
-    end
-
-    it 'does not show application of other users to non-admin users' do
-      user = make_user
-      other_user = make_user(email: 'other@email.com')
-      sign_in_as(user)
-
-      event = make_event
-      application = make_application(event, applicant_id: other_user.id)
-
-      get :show, params: { event_id: event.id, id: application.id }
-
-      assert_redirected_to root_path
-    end
-
-    it 'shows submitted application to admin users' do
-      admin = make_admin
-      sign_in_as(admin)
-
-      event = make_event
-      application = make_application(event)
-
-      get :show, params: { event_id: event.id, id: application.id }
-
-      assert_response :success
-    end
-
-    it 'redirects admin users to root if they try to see drafted applications' do
-      admin = make_admin
-      sign_in_as(admin)
-
-      event = make_event
-      draft = make_draft(event)
-
-      get :show, params: { event_id: event.id, id: draft.id }
-
-      assert_redirected_to root_path
-    end
-
-    it 'raise exception if application does not exist' do
-      admin = make_admin
-      sign_in_as(admin)
-
-      event = make_event
-
-      assert_raises(ActiveRecord::RecordNotFound) do
-        get :show, params: { event_id: event.id, id: 1 }
-      end
-    end
-
-    it 'redirects users to applications overview if the event has been deleted' do
-      user = make_user
-      event = make_event(organizer_id: user.id)
-      application = make_application(event, applicant_id: user.id)
-      event.update_attributes(start_date: 1.week.ago, end_date: 1.week.ago)
-
-      sign_in_as(user)
-
-      get :show, params: { event_id: event.id, id: application.id }
-
-      applications_controller = @controller
-
-      @controller = EventsController.new
-      delete :destroy, params: { id: event.id }
-
-      @controller = applications_controller
-      get :show, params: { event_id: event.id, id: application.id }
-
-      assert_redirected_to user_applications_path(user.id)
-      assert_equal "You cannot view your application as the event you applied for has been removed from Diversity Tickets", flash[:alert]
-    end
-  end
-
   describe '#new' do
     it 'redirects to the event if the application process is run by the organizer' do
       event = make_event(application_process: 'application_by_organizer',
@@ -182,6 +101,88 @@ class ApplicationsControllerTest < ActionController::TestCase
       assert_equal "You have already applied for #{event.name}", flash[:alert]
     end
   end
+
+  describe '#show' do
+    it 'does not show application to visitors' do
+      event = make_event
+      application = make_application(event)
+
+      get :show, params: { event_id: event.id, id: application.id }
+
+      assert_redirected_to sign_in_path
+    end
+
+    it 'does not show application of other users to non-admin users' do
+      user = make_user
+      other_user = make_user(email: 'other@email.com')
+      sign_in_as(user)
+
+      event = make_event
+      application = make_application(event, applicant_id: other_user.id)
+
+      get :show, params: { event_id: event.id, id: application.id }
+
+      assert_redirected_to root_path
+    end
+
+    it 'shows submitted application to admin users' do
+      admin = make_admin
+      sign_in_as(admin)
+
+      event = make_event
+      application = make_application(event)
+
+      get :show, params: { event_id: event.id, id: application.id }
+
+      assert_response :success
+    end
+
+    it 'redirects admin users to root if they try to see drafted applications' do
+      admin = make_admin
+      sign_in_as(admin)
+
+      event = make_event
+      draft = make_draft(event)
+
+      get :show, params: { event_id: event.id, id: draft.id }
+
+      assert_redirected_to root_path
+    end
+
+    it 'raise exception if application does not exist' do
+      admin = make_admin
+      sign_in_as(admin)
+
+      event = make_event
+
+      assert_raises(ActiveRecord::RecordNotFound) do
+        get :show, params: { event_id: event.id, id: 1 }
+      end
+    end
+
+    it 'redirects users to applications overview if the event has been deleted' do
+      user = make_user
+      event = make_event(organizer_id: user.id)
+      application = make_application(event, applicant_id: user.id)
+      event.update_attributes(start_date: 1.week.ago, end_date: 1.week.ago)
+
+      sign_in_as(user)
+
+      get :show, params: { event_id: event.id, id: application.id }
+
+      applications_controller = @controller
+
+      @controller = EventsController.new
+      delete :destroy, params: { id: event.id }
+
+      @controller = applications_controller
+      get :show, params: { event_id: event.id, id: application.id }
+
+      assert_redirected_to user_applications_path(user.id)
+      assert_equal "You cannot view your application as the event you applied for has been removed from Diversity Tickets", flash[:alert]
+    end
+  end
+
 
   describe '#update' do
     it 'updates attributes of application' do
---
+++ test/controllers/drafts_controller_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class DraftsControllerTest < ActionController::TestCase
+  describe '#create' do
+    it 'creates drafts for applications for a certain event' do
+      user = make_user
+      event = make_event
+
+      sign_in_as(user)
+
+      get :create, params: { event_id: event.id, application: { applicant_id: user.id } }
+
+      assert_equal Application.last.applicant_id, user.id
+      assert_equal Application.last.event, event
+    end
+
+    it 'does not allow to create drafts if the user is not logged in' do
+      user = make_user
+      event = make_event
+
+      get :create, params: { event_id: event.id, application: { applicant_id: user.id } }
+
+      assert_redirected_to sign_in_path
+    end
+  end
+
+  describe '#update' do
+    it 'updates drafts attributes' do
+      user = make_user
+      event = make_event
+      draft = make_draft(event, { applicant_id: user.id })
+
+      sign_in_as(user)
+
+      post :update, params: { event_id: event.id, id: draft.id, application: { name: "New name" } }
+      draft.reload
+
+      assert_equal draft.name, "New name"
+    end
+  end
+end
