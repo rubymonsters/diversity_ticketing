7b6dc2754cc42c2a4c7d8ff2c92f0e5ca1fe1eab
mauditecandela <candela.jimenez@gmail.com>
2018-07-04 14:15:08 +0200

    Refactors Drafts controller Adds applicant_id only in the creation Deletes que conditional if the application was not updated, because due to the lack of validations this is always happening

--- app/controllers/drafts_controller.rb
+++ app/controllers/drafts_controller.rb
@@ -1,12 +1,10 @@
 class DraftsController < ApplicationController
   before_action :get_event
 
-  def new
-  end
-
   def create
     @draft = Application.new(application_params)
     @draft.event = @event
+    set_applicant_id
     save_draft("You have successfully saved an application draft for #{@event.name}.")
   end
 
@@ -15,14 +13,11 @@ class DraftsController < ApplicationController
     @draft.skip_validation = true
     if @draft.update(application_params)
       save_draft("You have successfully saved your changes to the draft.")
-    else
-      redirect_to edit_event_applications_path(@event.id, @draft.id)
     end
   end
 
   private
   def application_params
-    set_applicant_id
     params.require(:application).permit(:name, :email, :email_confirmation, :attendee_info_1,
     :attendee_info_2, :visa_needed, :terms_and_conditions, :applicant_id)
   end
