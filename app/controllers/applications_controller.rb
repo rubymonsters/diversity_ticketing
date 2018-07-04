983bf2b87473505f43aa7107c2e63ff39b77ee51
mauditecandela <candela.jimenez@gmail.com>
2018-07-04 14:20:16 +0200

    Adds applicant_id only in the creation

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -22,6 +22,7 @@ class ApplicationsController < ApplicationController
     else
       @application = Application.new(application_params)
       @application.event = @event
+      set_applicant_id
       if @application.save
         submit
       else
@@ -80,7 +81,6 @@ class ApplicationsController < ApplicationController
 
   private
   def application_params
-    set_applicant_id
     params.require(:application).permit(:name, :email, :email_confirmation, :attendee_info_1,
     :attendee_info_2, :visa_needed, :terms_and_conditions, :applicant_id)
   end
