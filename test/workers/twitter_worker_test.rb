3250ab1523c145def0597cb25f6f91144b4d87c6
mauditecandela <candela.jimenez@gmail.com>
2018-07-17 14:43:58 +0200

    Makes test pass

--- test/workers/twitter_worker_test.rb
+++ test/workers/twitter_worker_test.rb
@@ -4,11 +4,13 @@ class TwitterWorkerTest < ActiveSupport::TestCase
   include ApplicationHelper
 
   it 'sends a tweet with name if event has no twitter handle' do
-    event = Event.new(
+    event = make_event(
       name: 'Awesome Event',
       id: 101,
       deadline: 2.weeks.from_now
     )
+
+    event.reload
 
     TWITTER_CLIENT.expects(:update).once
 
@@ -16,12 +18,14 @@ class TwitterWorkerTest < ActiveSupport::TestCase
   end
 
   it 'sends a tweet with twitter handle if event has a twitter handle' do
-    event = Event.new(
+    event = make_event(
       name: 'Awesome Event',
       id: 101,
       deadline: 2.weeks.from_now,
       twitter_handle: 'awesome_event'
     )
+
+    event.reload
 
     TWITTER_CLIENT.expects(:update).once
 
@@ -29,11 +33,13 @@ class TwitterWorkerTest < ActiveSupport::TestCase
   end
 
   it 'uses shortened name if name is longer than 30 characters' do
-     event = Event.new(
+     event = make_event(
       name: 'Super Duper Mega Crazy Awesome Event',
       id: 101,
       deadline: 2.weeks.from_now
     )
+
+    event.reload
 
     TWITTER_CLIENT.expects(:update).once
 
