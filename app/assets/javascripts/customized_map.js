d268cf865811cc3006fc6d46bb671fc81fd27437
mauditecandela <candela.jimenez@gmail.com>
2018-07-25 15:59:26 +0200

    Improves hoverstyling

--- app/assets/javascripts/customized_map.js
+++ app/assets/javascripts/customized_map.js
@@ -30,7 +30,7 @@
         highlightBorderColor: '#27AAE1',
         highlightFillColor: '#65BE66',
         popupTemplate: function(geography, data) {
-          return '<div class="hoverinfo">' + geography.properties.name + ' <p>Number of events: ' +  data.numberOfEvents + ' <p>Number of tickets: ' + data.numberOfTickets
+          return '<div class="hoverinfo"> <p class="hovertitle">' + geography.properties.name + '</p><p>Number of events: ' +  data.numberOfEvents + ' <p>Number of tickets: ' + data.numberOfTickets
         },
         highlightBorderWidth: 1
       },
