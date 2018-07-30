86b9d438f02dac1509d154e983b869d7178a6fe4
mauditecandela <candela.jimenez@gmail.com>
2018-07-30 16:23:48 +0200

    Adds locale picker

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :require_login
+  before_action :set_locale
 
+  def build_missing_translations(object)
+    I18n.available_locales.each do |locale|
+      object.translations.build(locale: locale) unless object.translated_locales.include?(locale)
+    end
+  end
+
+  def set_locale
+    desired_locale = request.headers['HTTP_ACCEPT_LANGUAGE'].to_s[0..1] == 'es' ? 'es' : 'en'
+    pp "PARAMS"
+    pp params[:locale]
+    pp "DESIRED LOCALE"
+    pp desired_locale
+    # I18n.locale = params[:locale] || desired_locale
+    pp I18n.locale
+    I18n.locale = :es
+  end
   def require_admin
     redirect_to root_path unless admin_user?
   end
