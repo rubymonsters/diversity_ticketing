151971ebb099f75dc80ba072128626799dc6bf6c
mauditecandela <candela.jimenez@gmail.com>
2018-07-30 16:23:28 +0200

    Adds locale setting

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,9 @@ module DiversityTicketing
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :en
+
     config.active_job.queue_adapter = :sidekiq
 
     # set default url options for mailers
