82c7a63df44a05312bf5580a088d84c013055042
Jessica Grzanna <jgrzanna@me.com>
2018-08-07 10:22:04 +0200

    adds validation for deadline to check that date is before or equal to end_date

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,8 +10,9 @@ class Event < ApplicationRecord
   has_many :interested_users, -> { distinct }, through: :tag_taggings, source: 'user'
 
   validates :organizer_name, :description, :name, :website, :code_of_conduct, :city, :country, presence: true, unless: :skip_validation
-  validates :start_date, :deadline, date: true, presence: true, unless: :skip_validation
+  validates :start_date, date: true, presence: true, unless: :skip_validation
   validates :end_date, date: { after_or_equal_to: :start_date }, presence: true, unless: :skip_validation
+  validates :deadline, date: { before_or_equal_to: :end_date }, presence: true, unless: :skip_validation
   validates :organizer_email, confirmation: true, format: { with: /.+@.+\..+/ }, presence: true, unless: :skip_validation
   validates :organizer_email_confirmation, presence: true, on: :create, unless: :skip_validation
   validates :website, :code_of_conduct, format: { with: /(http|https):\/\/.+\..+/ }, unless: :skip_validation
