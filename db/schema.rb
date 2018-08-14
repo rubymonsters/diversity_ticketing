94968976b01e64e78732ebee803b493ace7436eb
mauditecandela <candela.jimenez@gmail.com>
2018-08-14 12:36:15 +0200

    Makes tests to pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2018_08_07_082714) do
+ActiveRecord::Schema.define(version: 2018_08_08_083204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
