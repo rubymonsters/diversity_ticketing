33ee560e1c224caf2d6c75a68186e2e31f8a5b2b
Jessica Grzanna <jgrzanna@me.com>
2018-08-27 14:02:52 +0200

    changes deadline_in_two_days method for events to deadline_in_three_days

--- app/services/deadline_mail_service.rb
+++ app/services/deadline_mail_service.rb
@@ -1,6 +1,6 @@
 class DeadlineMailService
   def self.send_deadline_mail
-    Event.approved.deadline_in_two_days.each do |event|
+    Event.approved.deadline_in_three_days.each do |event|
       User.admin.each do |user|
         AdminMailer.upcoming_event_deadline(event, user.email).deliver_later
       end
@@ -8,7 +8,7 @@ class DeadlineMailService
   end
 
   def self.send_deadline_reminder_applicants
-    Event.approved.deadline_in_two_days.each do |event|
+    Event.approved.deadline_in_three_days.each do |event|
       event.applications.where(submitted: false).each do |application|
         ApplicantMailer.deadline_reminder(application).deliver_later
       end
