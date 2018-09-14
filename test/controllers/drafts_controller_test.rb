88d0ccf10ad834b872a03e50a59be2060b82ac8c
mauditecandela <candela.jimenez@gmail.com>
2018-09-14 13:01:17 +0200

    Update drafts controller tests

--- test/controllers/drafts_controller_test.rb
+++ test/controllers/drafts_controller_test.rb
@@ -41,4 +41,20 @@ class DraftsControllerTest < ActionController::TestCase
       assert_equal draft.name, 'New name'
     end
   end
+
+  describe '#submit' do
+    it 'submits the draft and transforms it into an application' do
+      user = make_user
+      event = make_event
+      draft = make_draft(event, applicant_id: user.id, email: user.email, email_confirmation: user.email)
+
+      sign_in_as(user)
+
+      post :submit, params: { event_id: event.id, id: draft.id, application: { email_confirmation: user.email } }
+
+      draft.reload
+
+      assert_equal draft.submitted, true
+    end
+  end
 end
