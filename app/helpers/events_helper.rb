82dfffcc4f2a78299543c55d22f5bda20911d77c
Jessica Grzanna <jgrzanna@me.com>
2019-05-22 17:30:15 +0200

    doc: Comment out event helper method
    I left this code on purpose, we might be able to use a modified version after more refactoring of the breadcrumbs

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -8,14 +8,17 @@ module EventsHelper
     end
   end
 
-  def breadcrumb_link_according_to_user_status
-    if current_user && @event.organizer_id == current_user.id
-      link_to 'Your Events', user_path(current_user)
-    elsif current_user && current_user.admin?
-      link_to 'Admin', admin_path
-    else
-      link_to 'Events', events_path
-    end
-  end
+  #Currently not used - Left it for further refactoring, maybe we can
+  #reuse this later if we modify it for the breadcrumb_partial:
+
+  # def breadcrumb_link_according_to_user_status
+  #   if current_user && @event.organizer_id == current_user.id
+  #     link_to 'Your Events', user_path(current_user)
+  #   elsif current_user && current_user.admin?
+  #     link_to 'Admin', admin_path
+  #   else
+  #     link_to 'Events', events_path
+  #   end
+  # end
 
 end
