1cbb6e950f76734377888941a8fdb7e151df5203
Jessica Grzanna <jgrzanna@me.com>
2019-08-20 09:20:13 +0200

    refactor: Code cleanup
    Gets rid of console.log

--- app/assets/javascripts/searchbar-input-width.js
+++ app/assets/javascripts/searchbar-input-width.js
@@ -1,6 +1,5 @@
 //Adjust searchbar input field width according to placeholder length
 $(document).ready(function() {
-  console.log($("#query.search-input")[0].placeholder.length);
   $(".search-input").width(
     $("input#query.search-input")[0].placeholder.length * 10
   );
